fda0420e5f5ec0e46756ea2c4fb6c3a24fdc153d
Daniel Allen <daniel.steven.allen@gmail.com>
2013-08-09 11:22:46 -0400

    Now returns list of _UnformattedIDs that each tag belongs to

--- src/App.js
+++ src/App.js
@@ -1,79 +1,106 @@
 Ext.define('CustomApp', {
-                extend: 'Rally.app.App',
-                componentCls: 'app',
-            
-                launch: function() {
-                    Ext.create('Rally.data.WsapiDataStore', {
-                        model: 'Tag',
-                        fetch: ['Name'],
-                        autoLoad: true,
-                        listeners: {
-                            load: this._retrieveArtifacts,
-                            scope: this
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+
+    launch: function() {
+        // Get a list of all tags
+        Ext.create('Rally.data.WsapiDataStore', {
+            model: 'Tag',
+            fetch: ['Name'],
+            autoLoad: true,
+            listeners: {
+                load: this._retrieveArtifacts,
+                scope: this
+            }
+        });
+    },
+    
+    _retrieveArtifacts: function(store, data) {
+        
+        this.gRecords = [];
+        this.gData = data;  // Contains data from all tags
+        console.log(this.gData);
+        
+        var that = this;
+        
+        Ext.Array.each(that.gData, function(tagRecord) {
+            that.gRecords.push({
+                Name: tagRecord.get('Name'),
+                OID: tagRecord.get('ObjectID'),
+                UsedIn: null
+            });    
+        });
+        
+       Ext.create('Rally.data.lookback.SnapshotStore', {
+            autoLoad: true,
+            listeners: {
+                load: this._onDataLoaded,
+                scope: this
+            },
+            fetch: ['_UnformattedID', 'Tags'],
+            hydrate: ['Tags'],
+            filters: [
+                 {
+                     property: '_TypeHierarchy',
+                     operator: 'in',
+                     value: ['Defect']
+                 },
+                 {
+                     property: 'Tags',
+                     operator: '!=',
+                     value: null
+                 },
+                 {
+                     property: 'Tags._PreviousValues',
+                     operator: '=',
+                     value: null
+                 }
+            ]
+        });
+    },
+
+    _onDataLoaded: function(store, data) {
+        
+        var that = this;
+        
+        // TODO: Does not remove duplicate UnformattedIDs, doesn't prefix UnformattedIDs with DE, US, TA
+        //       Need to check for null and remove it before appending UnformattedIDs to the UsedIn field
+        Ext.Array.each(data, function(artifactRecord) {
+            var recordTags = artifactRecord.get('Tags');
+            var artifactIDs = [];
+            var id = artifactRecord.get('_UnformattedID');
+            for(var i = 0; i < recordTags.length; i++) {
+                for (var j = 0; j < that.gRecords.length; j++) {
+                    if (that.gRecords[j].OID === recordTags[i]) {
+                        if (!Ext.Array.contains(artifactIDs, id)) {
+                            that.gRecords[j].UsedIn += (id + ", ");
+                            Ext.Array.push(id);
                         }
-                    });
-                },
-                
-                _retrieveArtifacts: function(store, data) {
-                    
-                    var records = [];
-                    Ext.Array.each(data, function(record) {
-                        records.push({
-                            Name: record.get('Name'),
-                            Creator: null,
-                            DateCreated: null,
-                            UsedIn: null
-                        });
-                        
-                    });
-                    
-                   /* Ext.create('Rally.data.WsapiDataStore', {
-                        autoLoad: true,
-                        listeners: {
-                            load: this._onDataLoaded,
-                            scope: this
-                        },
-                        filters: [
-                             {
-                                 property: '_TypeHierarchy',
-                                 operator: 'in',
-                                 value: ['Artifact']
-                             }   
-                        ],
-                    });
-                },
-            
-                _onDataLoaded: function(store, data) {
-                    var records = [];
-                    Ext.Array.each(data, function(record) {
-                        //Perform custom actions with the data here
-                        //Calculations, etc.
-                        records.push({
-                            Name: record.get('Name'),
-                        });
-                        
-                    });*/
-            
-                    this.add({
-                        xtype: 'rallygrid',
-                        store: Ext.create('Rally.data.custom.Store', {
-                            data: records,
-                            pageSize: 5
-                        }),
-                        columnCfgs: [
-                            {
-                                text: 'Name', dataIndex: 'Name', flex: 1
-                            },
-                            {
-                                text: 'Added By', dataIndex: 'Creator'
-                            },
-                            {
-                                text: 'Created On', dataIndex: 'DateCreated'
-                            },
-                            {
-                                text: 'Associated Artifacts', dataIndex: 'UsedIn'
-                            }
-                        ]
-                    });
+                    }
                 }
-            });
+            }
+        });
+
+        this.add({
+            xtype: 'rallygrid',
+            store: Ext.create('Rally.data.custom.Store', {
+                data: that.gRecords,
+                pageSize: 20
+            }),
+            columnCfgs: [
+                {
+                    text: 'Name', dataIndex: 'Name', flex: 1
+                },
+                {
+                    text: 'Added By'//, dataIndex: 'Creator'
+                },
+                {
+                    text: 'Created On'//, dataIndex: 'DateCreated'
+                },
+                {
+                    text: 'Associated Artifacts', dataIndex: 'UsedIn'
+                }
+            ]
+        });
+    }
+});
